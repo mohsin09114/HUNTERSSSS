6b2678e85c9f846469599df6acaaf6d6c39a7796
Mohsin <101172197+mohsin09114@users.noreply.github.com>
2023-06-17 18:20:21 +0500

    Update HUNTERS.py

--- HUNTERS.py
+++ HUNTERS.py
@@ -171,8 +171,8 @@ class Main:
     """)
 		print("%s [%s•%s] %sTOOL NAME : %s2009 Cloning Tool"%(G,R,G,B,G))
 		print("%s [%s•%s] %sVERSION   : %s6"%(G,R,G,B,G))
-		print("%s [%s•%s] %sYOUR KEY  : %s%s"%(G,R,G,B,G,key))
-		print("%s [%s•%s] %sSTATUS    : %s"%(G,R,G,B,stat)) 
+		print("%s [%s•%s] %sYOUR KEY  : %s%s"MOHSIN=193010
+		print("%s [%s•%s] %sSTATUS    : %s"%(paid user)) 
 		print("───────────────────────────────────────────────────────× ")
 		print(" \t\033[1;97m[\x1b[1;97m\x1b[1;41m JOIN MY WHATSPP GROUP FIRST\x1b[0m\x1b[1;97m] \033[1;37m ")
 		subprocess.check_output(["am", "start", "https://chat.whatsapp.com/HTKsJexrFa8AIZX7wvrMQo"])
